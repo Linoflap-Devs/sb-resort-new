2739c5fc6d170172f18ebca325c9ce710e3be9c2
WRavagoLFT <wendell.ravago@linoflaptech.com>
2025-03-26 10:00:38 +0800

    fix for sorting button column type

--- components/SortingButton.tsx
+++ components/SortingButton.tsx
@@ -1,6 +1,6 @@
 import { ArrowUp, ArrowDown } from "lucide-react";
 
-const SortingButton = ({ column: any }) => {
+const SortingButton = ({ column }: any) => {
   const sortedDirection = column.getIsSorted();
 
   return (
